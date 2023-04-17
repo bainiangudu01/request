40b494ddac66f30d8c0fe9d6f58e7b64d0e544a0
1379225776@qq.com <1379225776@qq.com>
2023-04-18 00:06:37 +0800

    更新数据

--- 数据结构与算法/算法进阶/算法练习.py
+++ 数据结构与算法/算法进阶/算法练习.py
@@ -1,5 +1,18 @@
-n=bin(int(input()))[2:].split('0')
-print(n)
-n.sort(key=lambda x:len(x),reverse=True)
-print(n)
-print(len(n[0]))
+s = input()
+d = ''  # 把s中的字符挑出来放到d中排序
+
+for i in s:
+    if i.isalpha():
+        d = d + i
+d = sorted(d, key=str.upper)  # 可以实现1）字符由A到Z的排序2）同字母（A与a算同字母），由输入先后顺序排列
+
+ind = 0
+f = ''  # 最终要输出的字符串
+
+for i in s:
+    if i.isalpha():  # 如果i是字符
+        f = f + d[ind]
+        ind += 1
+    else:
+        f = f + i
+print(f)
