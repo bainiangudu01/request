6a0ab7b33ca8d41b567f6a3a30720330e883e8f5
1379225776@qq.com <1379225776@qq.com>
2023-04-22 08:54:29 +0800

    Merge remote-tracking branch 'request/master'

--- 百度工作/算法练习.py
+++ 百度工作/算法练习.py
@@ -1,54 +1,32 @@
 while True:
     try:
-        m, n = map(int, input().split())
-        # 初始化表格
-        if m <= 9 and n <= 9:
-            print(0)
-            l = [[0 for i in range(n)] for i in range(m)]
+        s = input()
+        if len(s) <= 8:
+            print("NG")
         else:
-            print(-1)
-
-        # 交换
-        x1, y1, x2, y2 = map(int, input().split())
-        if (x1 in range(m) and x2 in range(m)) and (y1 in range(n) and y2 in range(n)):
-            l[x1][y1], l[x2][y2] = l[x2][y2], l[x1][y1]
-            print(0)
-        else:
-            print(-1)
-        # 插入行
-        x = int(input())
-        l.insert(x, [0 for i in range(n)])
-        if len(l) > 9:
-            print(-1)
-        else:
-            print(0)
-            l.pop()
-        # 插入列
-        y = int(input())
-        if n==9 or y>=n:
-            print(-1)
-        else:
-            for i in range(m):
-                l[i].insert(y, 0)
-            for i in range(m):
-                if len(l[i]) > 9 or len(l[i]) > n + 1:
-                    print(-1)
-                    break
+            upp, low, dig, sym = 0, 0, 0, 0
+            for i in s:
+                if i.upper():
+                    upp = 1
+                elif i.lower():
+                    low = 1
+                elif i.isdigit():
+                    dig = 1
+                elif i.isascii():
+                    sym = 1
+            if upp + low + dig + sym < 3:
+                print("NG")
+            else:
+                for i in range(len(s) - 3):
+                    if len(s.split(s[i:i + 3])) >= 3:
+                        print("NG")
+                        print("NG")
+                        break
                 else:
-                    print(0)
-                    for i in range(m):
-                        l[i].pop()
-                    break
-        # 查询
-        x, y = map(int, input().split())
-        if x <= m - 1 and y <= n - 1:
-            print(0)
-        else:
-            print(-1)
+                    print("OK")
+
+
+
+
     except:
         break
-# 7 3
-# 2 2 4 0
-# 4
-# 4
-# 6 2
--- 百度工作/算法练习1.py
+++ 百度工作/算法练习1.py
@@ -1,13 +1,3 @@
-while True:
-    try:
-        m, n = map(int, input().split())
-        x1, y1, x2, y2 = map(int, input().split())
-        x, y = int(input()), int(input())
-        x3, y3 = map(int, input().split())
-        print(0 if m <= 9 and n <= 9 else -1)
-        print(0 if x1 < m and x2 < m and y1 < n and y2 < n else -1)
-        print(0 if 9 > m > x else -1)
-        print(0 if 9 > n > y else -1)
-        print(0 if x3 < m and y3 < n else -1)
-    except:
-        break
+a="ABCABC"
+b=a.split('ABC')
+print(b)
