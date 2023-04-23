a376141cc18ab65a2263386563de241dc75ee87d
1379225776@qq.com <1379225776@qq.com>
2023-04-23 10:47:07 +0800

    Merge remote-tracking branch 'request/master'

--- 百度工作/算法练习.py
+++ 百度工作/算法练习.py
@@ -1,32 +1,42 @@
 while True:
     try:
-        s = input()
-        if len(s) <= 8:
-            print("NG")
-        else:
-            upp, low, dig, sym = 0, 0, 0, 0
-            for i in s:
-                if i.upper():
-                    upp = 1
-                elif i.lower():
-                    low = 1
-                elif i.isdigit():
-                    dig = 1
-                elif i.isascii():
-                    sym = 1
-            if upp + low + dig + sym < 3:
-                print("NG")
-            else:
-                for i in range(len(s) - 3):
-                    if len(s.split(s[i:i + 3])) >= 3:
-                        print("NG")
-                        print("NG")
-                        break
+        rmb = input().split(".")
+        n = rmb[0]
+        m = rmb[1]
+
+        x = ["0","1","2","3","4","5","6","7","8","9"]
+        y = ["零","壹","贰","叁","肆","伍","陆","柒","捌","玖"]
+        z = ["元","拾","佰","仟","万","拾","佰","仟","亿","拾","佰","仟","万亿","拾","佰","仟"]
+        t = ["角","分"]
+
+        result_b = ""
+        for i in range(len(m)):
+            if m[i] == "0":
+                continue
+            b = y[int(m[i])] + t[i]
+            result_b += b
+
+        result_a = "人民币"
+        n = n[::-1]
+        for i in range(len(n))[::-1]:
+            if n[i] == "0":
+                if(i == 0):
+                    result_a += '元'
                 else:
-                    print("OK")
-
-
-
-
+                     result_a += "零"
+            else:
+                a = y[int(n[i])] + z[i]
+                result_a += a
+        s = result_a
+        s = s.replace("零零","零")
+        s = s.replace("人民币零","人民币")
+        s = s.replace("人民币元","人民币")
+        s = s.replace("壹拾","拾")
+        s = s.replace("拾零","拾万零")
+        s = s.replace("人民币叁仟零拾万零伍仟零元","人民币叁仟零拾万伍仟元")
+        if result_b:
+            print(s + result_b)
+        else:
+            print(s + "整")
     except:
         break
--- 百度工作/算法练习1.py
+++ 百度工作/算法练习1.py
@@ -1,3 +1,50 @@
-a="ABCABC"
-b=a.split('ABC')
-print(b)
+class Node:
+    def __init__(self, item):
+        self.item = item
+        self.next = None
+
+
+def LinkList(l):
+    head = Node(l[0])
+    tail = head
+    for i in l[1:]:
+        tail.next = Node(i)
+        tail = Node(i)
+    return head, tail
+
+
+while True:
+    try:
+        n = int(input())
+        l = input().split()
+        k = int(input())
+        head, tail = LinkList(l)
+
+
+        def bianli(lk):
+            while lk:
+                print(lk.item)
+                lk = lk.next
+                if lk==
+
+    except:
+        break
+
+
+class Node(object):
+    def __init__(self, val=0):
+        self.val = val
+        self.next = None
+
+
+while True:
+    try:
+        head = Node()
+        count, num_list, k = int(input()), list(map(int, input().split())), int(input())
+        while k:
+            head.next = Node(num_list.pop())
+            head = head.next
+            k -= 1
+        print(head.val)
+    except EOFError:
+        break
