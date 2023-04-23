6da4c99340cbb1f5a88f5b2084a980001b2053b1
zhang--yang01 <1379225776@qq.com>
2023-04-23 19:43:24 +0800

    Merge remote-tracking branch 'origin/master'

--- 百度工作/文件上传请求.py
+++ 百度工作/文件上传请求.py
@@ -4,13 +4,13 @@ headers = {
     "Authorization": "v_zhangyang30",
     "accept": "*/*",
 }
-f = open("/Users/v_zhangyang30/Downloads/11.7.0.18.apk", "rb")
+f = open("/Users/v_zhangyang30/Downloads/11.7.0.22.apk", "rb")
 resp = requests.post(
     "https://quick.baidu-int.com/api/file/app",
     headers=headers,
     files={"file": f},
     data={
-        "branchName": "release_11_7_0_0",
+        "branchName": "feature_196_merge_koom",
         "productId": 1347058356187308033,
         "type": "apk",
     }, )
