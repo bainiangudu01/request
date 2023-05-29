bd0c1e6677904ecf792823de2fb4174c531a4ce4
1379225776@qq.com <1379225776@qq.com>
2023-05-30 01:40:22 +0800

    Merge remote-tracking branch 'request/master'

--- 百度工作/文件上传请求.py
+++ 百度工作/文件上传请求.py
@@ -4,13 +4,13 @@ headers = {
     "Authorization": "v_zhangyang30",
     "accept": "*/*",
 }
-f = open("/Users/v_zhangyang30/Downloads/11.7.0.22.apk", "rb")
+f = open("C:/Users/13792/Desktop/下载内容/输入法/10.6.121.82wd.apk", "rb")
 resp = requests.post(
     "https://quick.baidu-int.com/api/file/app",
     headers=headers,
     files={"file": f},
     data={
-        "branchName": "feature_196_merge_koom",
+        "branchName": "dev_oem_10_6_121_0_xiaomi_new",
         "productId": 1347058356187308033,
         "type": "apk",
     }, )
