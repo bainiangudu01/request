624d3abd62d78389a03bbe3b3d803927a0e8705d
zhang--yang01 <1379225776@qq.com>
2023-05-31 16:13:06 +0800

    Merge remote-tracking branch 'origin/master'

--- 百度工作/文件上传请求.py
+++ 百度工作/文件上传请求.py
@@ -4,13 +4,13 @@ headers = {
     "Authorization": "v_zhangyang30",
     "accept": "*/*",
 }
-f = open("C:/Users/13792/Desktop/下载内容/输入法/10.6.121.82wd.apk", "rb")
+f = open("/Users/v_zhangyang30/Downloads/11.7.0.24.apk", "rb")
 resp = requests.post(
     "https://quick.baidu-int.com/api/file/app",
     headers=headers,
     files={"file": f},
     data={
-        "branchName": "dev_oem_10_6_121_0_xiaomi_new",
+        "branchName": "release_11_7_0_0",
         "productId": 1347058356187308033,
         "type": "apk",
     }, )
